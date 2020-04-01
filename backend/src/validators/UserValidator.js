291ed9b154ec07b9f5c326606f5ccce53913723c
LCOI <lucas.c968@gmail.com>
2020-04-01 17:20:11 -0300

    User validator file renamed; Added detail validator

---
+++ backend/src/validators/UserValidator.js
@@ -0,0 +1,22 @@
+const { Segments, Joi } = require('celebrate');
+
+module.exports = {
+
+    create: {
+        [Segments.BODY]:   Joi.object().keys({
+            name:          Joi.string().required(),
+            id_number:     Joi.number().integer().required(),
+            login:         Joi.string().required().min(3),
+            password:      Joi.string().required().min(6),
+            email:         Joi.string().required().email(),
+            department_id: Joi.number().integer().required()
+        })
+    },
+
+    detail: {
+        [Segments.PARAMS]: Joi.object().keys({
+            user_id: Joi.number().integer().required()
+        })
+    }
+
+}
