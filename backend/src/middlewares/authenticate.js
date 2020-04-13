02a5b691d6864dcff5abf6235cef9867a9824d72
LCOI <lucas.c968@gmail.com>
2020-04-13 08:13:21 -0300

    Added try-catch blocks and returning IsAuthenticated bool in json response

--- backend/src/middlewares/authenticate.js
+++ backend/src/middlewares/authenticate.js
@@ -2,18 +2,37 @@ const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth.json');
 
 module.exports = (req, res, next) => {
-	const token = req.headers.authorization;
+	try
+	{
+		const token = req.headers.authorization;
 
-	if (!token){
-		return res.status(401).send({ error: 'No token provided' });
-	}
-
-	jwt.verify(token, authConfig.secret, (err, decoded) => {
-		if (err){
-			return res.status(401).send({ error: 'Invalid token' });
+		if (!token){
+			return res.status(401).send({
+				isAuthenticated: false,
+				message: 'No token provided'
+			});
 		}
 
-		req.user_id = decoded.id;
-		return next();
-	});
+		jwt.verify(token, authConfig.secret, (err, decoded) => {
+			if (err){
+				return res.status(401).send({
+					isAuthenticated: false,
+					message: 'Invalid token'
+				});
+			}
+
+			req.user_id = decoded.user_id;
+			req.isAuthenticated = true;
+			return next();
+		});
+	}
+	catch (error)
+	{
+		console.log(error);
+		return res.status(500).json({
+			isAuthenticated: false,
+			message: 'An unexpected error has occured, please try again later'
+		});
+	}
+
 };
