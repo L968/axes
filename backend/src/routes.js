f74175f89e2d395d0592a558899f698b60d19a89
LCOI <lucas.c968@gmail.com>
2020-04-18 21:47:11 -0300

    Added resource routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,7 @@ const authenticate = require('./middlewares/authenticate');
 
 // Controllers
 const UserController = require('./controllers/UserController');
+const ResourceController = require('./controllers/ResourceController');
 const SessionController = require('./controllers/SessionController');
 
 // Validators
@@ -17,6 +18,9 @@ const SessionValidator = require('./validators/SessionValidator');
 routes.get('/user', UserController.index);
 routes.get('/user/:user_id', celebrate(UserValidator.detail), UserController.detail);
 routes.post('/user', celebrate(UserValidator.create), UserController.create);
+
+routes.get('/resource', ResourceController.index);
+routes.post('/resource', ResourceController.create);
 
 routes.post('/session', celebrate(SessionValidator.create), SessionController.create);
 
