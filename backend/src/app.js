9dd33322bb842800eb0126e225d54698761a6079
LCOI <lucas.c968@gmail.com>
2020-06-03 20:16:50 -0300

    added express-async-errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 const cors = require('cors');
 const routes = require('./routes');
 const logger = require('./logs/logger');
@@ -10,18 +11,11 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use((err, req, res, next) => {
+app.use(async (err, req, res, next) => {
     logger.error(err);
 
     return res.status(500).json({ message: 'An unexpected error has occured, please try again later' });
 });
 
-process.on('unhandledRejection', (reason, promise) => {
-    logger.error(reason);
-
-    setInterval(() => {
-        process.exit(1);
-    }, 100);
- });
 
 module.exports = app;
